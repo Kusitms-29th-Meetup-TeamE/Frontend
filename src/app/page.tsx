babc989733ec185bd986275dad98c7329b85a9be
chamny20 <chamny20@naver.com>
2024-04-24 22:48:12 +0900

    ✅ Test(#3): test font variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,19 @@ export default function Home() {
   return (
     <main>
       <h3>hello world</h3>
+      <div>안녕하세요 원티드 산스입니다.</div>
+      <div className="text-h1">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-h2">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-h3">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-h4">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-body1">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-body2">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-chip-bold">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-chip-medium">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-footer-bold">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-footer-medium">안녕하세요 원티드 산스입니다.</div>
+      <div className="text-footer-regular">안녕하세요 원티드 산스입니다.</div>
+      <div>안녕하세요 원티드 산스입니다.</div>
     </main>
   );
 }
