e606ec5eab674dc58d5063445644f77ccb420079
hyunn522 <tjgus52213@gmail.com>
2024-04-30 21:32:07 +0900

    🔨 Fix: replace logo image as svg

--- src/components/common-components/common/Footer.tsx
+++ src/components/common-components/common/Footer.tsx
@@ -1,9 +1,19 @@
 'user client';
 
+import Image from 'next/image';
+
 const Footer = () => {
   return (
-    <footer className="w-full fixed bottom-0 flex flex-col justify-center text-center bt-0 pt-[34px] pb-[34px] text-gray-06 bg-gray-03">
-      <div>또바</div>
+    <footer className="w-full fixed bottom-0 flex flex-col flex-wrap justify-center content-center bt-0 pt-[34px] pb-[34px] text-gray-06 bg-gray-03">
+      <div className="w-fit flex gap-[2px]">
+        <Image
+          src="/assets/ddoba_logo_black-footer.svg"
+          alt="logo"
+          width={32}
+          height={26}
+        />
+        <div>또바</div>
+      </div>
     </footer>
   );
 };
