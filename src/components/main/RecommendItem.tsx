70bc82ce18f8bd171dfd3f979f2d510b07366cae
chamny20 <dge3179@ajou.ac.kr>
2024-04-30 11:26:55 +0900

    🌈 Update(#8): add hover event

--- src/components/main/RecommendItem.tsx
+++ src/components/main/RecommendItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Tag from '../common-components/tag';
 
 export type RecommendItemProps = {
@@ -11,30 +13,34 @@ export type RecommendItemProps = {
 };
 
 export default function RecommendItem(props: RecommendItemProps) {
-  const { title, subTitle, totalNum, joinNum, date, img } = props;
+  const { title, subTitle, totalNum, joinNum, date, img, likeStatus } = props;
+
+  const [hover, setHover] = useState<boolean>(false);
 
   return (
-    <div className="flex flex-col border border-black m-2">
+    <div
+      className="flex flex-col border border-black cursor-pointer w-full"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <div className="rounded-[20px] w-full h-[380px] border border-red-500">
         이미지{img}
       </div>
       <div className="flex flex-col mt-[14px]">
         <div className="flex items-center justify-between mb-[12px] border border-green-500">
           <Tag color="orange" text="잔잔한" />
-          <div>하트 아이콘</div>
+          <div>{likeStatus ? '' : ''}하트 아이콘</div>
         </div>
         <div className="flex justify-between">
           <div className="flex flex-col gap-[9px]">
             <span className="text-gray-11 text-body2">{title}</span>
             <span className="text-gray-07 text-h5">{subTitle}</span>
           </div>
-          {/* hover하면 보여야 하는 부분 */}
-          {true ? (
+
+          {hover && (
             <div className="text-gray-11 text-h5">
               참여 인원: {joinNum}/{totalNum}
             </div>
-          ) : (
-            <></>
           )}
         </div>
 
