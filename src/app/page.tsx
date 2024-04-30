6fbaf898353c6fa82225a681dca5ed21dc8a678d
hyunn522 <tjgus52213@gmail.com>
2024-04-30 22:29:24 +0900

    ✅ Test: test footer not hiding content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
   return (
     <main>
       <h3>hello world</h3>
+      <div className="w-full h-[700px] bg-slate-200" />
+      <h3>end</h3>
     </main>
   );
 }
--- src/components/common-components/common/Footer.tsx
+++ src/components/common-components/common/Footer.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Footer = () => {
   return (
     <footer className="w-full h-[264px]  fixed bottom-0 flex flex-col flex-wrap justify-center content-center items-center text-gray-06 bg-gray-03">
-      <div className="w-fit flex gap-[2px] text-body1">
+      <div className="flex gap-[2px] text-body1">
         <Image
           src="/assets/ddoba_logo_black-footer.svg"
           alt="logo"
@@ -14,7 +14,7 @@ const Footer = () => {
         />
         <span>또바</span>
       </div>
-      <span className="w-fit flex mt-[15px] text-center text-chip-medium">
+      <span className="flex mt-[15px] text-center text-chip-medium">
         나중에 수정 예정,<br></br>새로운 만남의 기회를 제공하는 서비스
       </span>
       <ul className="flex gap-[40px] items-center mt-[24px] text-footer-regular">
