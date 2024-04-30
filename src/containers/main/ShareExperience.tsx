3512a9f5f1e7113fb32cda025c0c04adcf1d13be
chamny20 <chamny20@naver.com>
2024-04-30 18:41:08 +0900

    📍 Feat(#8): add components about share containers

---
+++ src/containers/main/ShareExperience.tsx
@@ -0,0 +1,12 @@
+import MainTitle from '@/components/main/MainTitle';
+
+export default function ShareExperience() {
+  return (
+    <div className="border border-black max-w-[1200px] w-full mx-auto">
+      <MainTitle
+        title="또바 추천 경험 나누기"
+        subTitle="우리 동네 선생님을 추천해드려요."
+      />
+    </div>
+  );
+}
