5dd8fe0a645b95f61f0e091c28476c6dc173c9c0
chamny20 <dge3179@ajou.ac.kr>
2024-05-02 10:19:34 +0900

    🌈 Update(#8): add temp img-url

--- src/containers/main/ShareExperience.tsx
+++ src/containers/main/ShareExperience.tsx
@@ -5,7 +5,7 @@ import ShareProfile from '@/components/main/ShareProfile';
 
 const data = [
   {
-    img: '/sdf',
+    img: '/assets/main/main_banner.png',
     name: '김또바',
     age: 24,
     gender: '남',
@@ -13,7 +13,7 @@ const data = [
     content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
   },
   {
-    img: '/sdf',
+    img: '/assets/main/main_banner.png',
     name: '김또바',
     age: 24,
     gender: '남',
@@ -21,7 +21,7 @@ const data = [
     content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
   },
   {
-    img: '/sdf',
+    img: '/assets/main/main_banner.png',
     name: '김또바',
     age: 24,
     gender: '남',
@@ -29,7 +29,7 @@ const data = [
     content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
   },
   {
-    img: '/sdf',
+    img: '/assets/main/main_banner.png',
     name: '김또바',
     age: 24,
     gender: '남',
@@ -40,7 +40,7 @@ const data = [
 
 export default function ShareExperience() {
   return (
-    <div className="border border-black max-w-[1200px] w-full mx-auto">
+    <div className="max-w-[1200px] w-full mx-auto">
       <MainTitle
         title="또바 추천 경험 나누기"
         subTitle="우리 동네 선생님을 추천해드려요."
