377a0ad5bdb17bbe7910adb8c50319154895a45f
chamny20 <dge3179@ajou.ac.kr>
2024-05-02 09:56:43 +0900

    🌈 Update(#8): apply type and props

--- src/components/main/ShareProfile.tsx
+++ src/components/main/ShareProfile.tsx
@@ -1,29 +1,36 @@
+import { UserProps } from '@/types/user';
+
 import Image from 'next/image';
 
-export default function ShareProfile() {
+export interface ShareProfileProps extends UserProps {
+  content: string;
+}
+
+export default function ShareProfile(props: ShareProfileProps) {
+  const { content, name, age, gender, address, img } = props;
+
   return (
     <div className="p-4 bg-white max-h-[400px] rounded-[20px] max-w-[282px] w-full shadow-[0_4px_30px_10px_rgba(0,0,0,0.08)]">
       <Image
-        src="/assets/main/how2.png"
+        // src="/assets/main/how2.png"
+        src={img ?? ''}
         alt=""
         width={250}
         height={250}
         className="rounded-xl border border-red-500"
       />
       <div className="flex justify-between mt-5">
-        <div className="text-footer-bold text-gray-09">김또바</div>
+        <div className="text-footer-bold text-gray-09">{name}</div>
         <div className="flex gap-[15px] px-[14px] py-1 bg-gray-03 rounded-[18px] text-footer-regular text-gray-08">
-          <span>60세</span>
+          <span>{age}세</span>
           <span>|</span>
-          <span>남</span>
+          <span>{gender}</span>
           <span>|</span>
-          <span>청계동</span>
+          <span>{address}</span>
         </div>
       </div>
       <div className="mt-[18px] text-gray-07 text-body3 border max-h-[52px] border-red-500 overflow-hidden text-ellipsis">
-        "아침마다 건강하게" "아침마다 건강하게" "아침마다 건강하하하하하게"
-        "아침마다 건강하게" "아침마다 건강하게" "아침마다 건강하게" "아침마다
-        건강하게"
+        {content}
       </div>
     </div>
   );
--- src/containers/main/ShareExperience.tsx
+++ src/containers/main/ShareExperience.tsx
@@ -1,4 +1,42 @@
+import { IoIosArrowForward } from 'react-icons/io';
+
 import MainTitle from '@/components/main/MainTitle';
+import ShareProfile from '@/components/main/ShareProfile';
+
+const data = [
+  {
+    img: '/sdf',
+    name: '김또바',
+    age: 24,
+    gender: '남',
+    address: '청계동',
+    content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
+  },
+  {
+    img: '/sdf',
+    name: '김또바',
+    age: 24,
+    gender: '남',
+    address: '청계동',
+    content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
+  },
+  {
+    img: '/sdf',
+    name: '김또바',
+    age: 24,
+    gender: '남',
+    address: '청계동',
+    content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
+  },
+  {
+    img: '/sdf',
+    name: '김또바',
+    age: 24,
+    gender: '남',
+    address: '청계동',
+    content: '안녕하세요 저는 실뜨기할래요 푸항항항항항',
+  },
+];
 
 export default function ShareExperience() {
   return (
@@ -6,7 +44,32 @@ export default function ShareExperience() {
       <MainTitle
         title="또바 추천 경험 나누기"
         subTitle="우리 동네 선생님을 추천해드려요."
+        className="mb-[50px]"
       />
+
+      <div className="flex gap-6">
+        {data.map((item, idx) => {
+          return (
+            <div key={idx}>
+              <ShareProfile
+                name={item.name}
+                age={item.age}
+                gender={item.gender}
+                content={item.content}
+                address={item.address}
+                img={item.img}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="mt-[70px] w-full mx-auto max-w-[1200px] text-center text-body3 text-gray-10">
+        <div className="inline-flex items-center gap-[2px] cursor-pointer">
+          더 많은 활동 보기
+          <IoIosArrowForward />
+        </div>
+      </div>
     </div>
   );
 }
