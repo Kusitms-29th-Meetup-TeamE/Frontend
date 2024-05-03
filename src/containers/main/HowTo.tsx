81134c106e5dd4162fb5fa7e47540ee67a085606
chamny20 <dge3179@ajou.ac.kr>
2024-05-03 11:51:52 +0900

    💄 Style(#14): update modified ui

--- src/containers/main/HowTo.tsx
+++ src/containers/main/HowTo.tsx
@@ -10,42 +10,53 @@ export default function HowTo() {
     {
       title: '내가 하고 싶은 활동을 찾고 있다면',
       imgUrl: '/assets/main/how1.png',
-      goToText: '활동 참여하기 바로가기',
-      goToUrl: '경험 나누기 바로가기',
+      goToText: '활동 참여하기',
+      goToUrl: '/',
     },
     {
       title: '알찬 내 인생을 위해 배움을 찾고 있다면',
       imgUrl: '/assets/main/how2.png',
-      goToText: '경험 나누기 바로가기',
-      goToUrl: '경험 나누기 바로가기',
+      goToText: '경험 나누기',
+      goToUrl: '/',
     },
     {
       title: '내 배움을 누군가에게 나누고 싶다면',
       imgUrl: '/assets/main/how3.png',
-      goToText: '경험 나누기 바로가기',
-      goToUrl: '경험 나누기 바로가기',
+      goToText: '함께 대화하기',
+      goToUrl: '/',
     },
   ];
 
   const router = useRouter();
 
   return (
-    <div className="mt-[-150px] w-full mx-auto max-w-[1435px] bg-white flex items-center justify-between rounded-full px-[140px] h-[300px] shadow-[0_4px_30px_10px_rgba(0,0,0,0.08)]">
+    <div className="w-full max-w-[1200px] flex gap-6">
       {howData.map((item, idx) => {
         return (
           <div
             key={idx}
-            className="text-center flex flex-col justify-center items-center"
+            className="bg-white p-[44px] box-border rounded-[40px] opacity-95 h-[250px] w-full max-w-[384px] shadow-[0_4px_30px_10px_rgba(0,0,0,0.08)]"
           >
-            <div className="text-primary-orange6 text-body2">{item.title}</div>
-            <Image src={item.imgUrl} alt="how-image" width={140} height={140} />
-            <div
-              className="bg-gray-03 cursor-pointer flex items-center gap-[2px] rounded-[20px] px-5 py-[10px] hover:bg-gray-04 hover:bg-opacity-70"
-              onClick={() => router.push(`${item.goToUrl}`)}
-            >
-              {item.goToText}
-              <IoIosArrowForward />
+            <div className="flex justify-between">
+              <Image
+                src={item.imgUrl}
+                alt="how-image"
+                width={120}
+                height={120}
+                className="mt-[-80px] object-cover"
+              />
+              <div className="flex items-center w-[36px] h-[36px] justify-center rounded-full bg-gray-chip cursor-pointer">
+                <IoIosArrowForward
+                  onClick={() => router.push(`${item.goToUrl}`)}
+                  color="#95989a"
+                />
+              </div>
             </div>
+
+            <p>{item.title}</p>
+            <span className="text-primary-orange6 text-h2">
+              {item.goToText}
+            </span>
           </div>
         );
       })}
