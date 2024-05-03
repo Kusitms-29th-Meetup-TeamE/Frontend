ccfd7869df5c2df5882c240fa512c2f14ff57bf3
hyunn522 <tjgus52213@gmail.com>
2024-05-03 18:10:14 +0900

    ✅ Test: main-bottom layout
    ISSUES CLOSED: #12

--- src/app/seohyun/page.tsx
+++ src/app/seohyun/page.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import PointItem from '@/components/main/PointItem';
+
 import Notice from '@/containers/main/Notice';
+import Point from '@/containers/main/Point';
 import SubBannerSlider from '@/containers/main/SubBannerSlider';
 
 const TestPage = () => {
   return (
-    <>
+    <div className="pr-[10px] pl-[10px]">
       <div className="flex gap-2 p-2"></div>
       <Notice />
       <div className="h-[30px]"></div>
-
       <SubBannerSlider
         imgs={[
           '/assets/main/subBanner1.svg',
@@ -17,7 +19,9 @@ const TestPage = () => {
           '/assets/main/subBanner1.svg',
         ]}
       />
-    </>
+      <div className="h-[30px]"></div>
+      <Point />
+    </div>
   );
 };
 
