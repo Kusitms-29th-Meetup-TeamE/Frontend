1d07890f14c06460ffcec424016ea158e5e8d62e
hyunn522 <tjgus52213@gmail.com>
2024-05-03 14:32:53 +0900

    ✅ Test: subBanner and notice

--- src/app/seohyun/page.tsx
+++ src/app/seohyun/page.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 import Notice from '@/containers/main/Notice';
+import SubBannerSlider from '@/containers/main/SubBannerSlider';
 
 const TestPage = () => {
   return (
     <>
       <div className="flex gap-2 p-2"></div>
       <Notice />
+      <div className="h-[30px]"></div>
+
+      <SubBannerSlider
+        imgs={[
+          '/assets/main/subBanner1.svg',
+          '/assets/main/subBanner1.svg',
+          '/assets/main/subBanner1.svg',
+        ]}
+      />
     </>
   );
 };
---
+++ src/containers/main/SubBannerSlider.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import 'swiper/css';
+import 'swiper/css';
+
+import React, { useState } from 'react';
+
+import Image from 'next/image';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+export type SubBannerSliderProps = {
+  imgs: string[];
+};
+
+const SubBannerSlider = (props: SubBannerSliderProps) => {
+  const { imgs } = props;
+  const [currentCard, setCurrentCard] = useState<number>(0);
+
+  return (
+    <div className="relative w-[1200px] mx-auto h-[146px] flex justify-center overflow-hidden ">
+      <Swiper
+        slidesPerView={'auto'}
+        loop={true}
+        centeredSlides={true}
+        initialSlide={currentCard}
+        onSlideChange={(swiper) => {
+          setCurrentCard(swiper.snapIndex);
+        }}
+        modules={[Pagination]}
+        pagination={{
+          clickable: true,
+          el: '.swiper-pagination',
+          type: 'bullets',
+          bulletClass: 'bg-gray-200 rounded-full',
+          bulletActiveClass: 'bg-white',
+        }}
+        className="h-full"
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {imgs?.map((item, index) => {
+          return (
+            <SwiperSlide key={index} className={'item-centered'}>
+              <Image src={item} fill alt="sub-banner" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className="absolute bottom-[22px] flex flex-col justify-end items-center w-full h-full">
+        <div className="swiper-pagination w-[36px] h-[4px]rounded-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SubBannerSlider;
