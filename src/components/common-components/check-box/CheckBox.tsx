7696a157d05fca0880f6e1a2e612de62c18cf5f4
hyunn522 <tjgus52213@gmail.com>
2024-05-04 22:01:36 +0900

    📍 Feat(#21): add check-box component

---
+++ src/components/common-components/check-box/CheckBox.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+export type CheckBoxProps = {
+  width: number;
+  height: number;
+  isLogin?: boolean;
+  isChecked: boolean;
+  handleCheck: () => void;
+};
+
+const CheckBox = (props: CheckBoxProps) => {
+  const { width, height, isLogin = false, isChecked, handleCheck } = props;
+
+  return (
+    <Image
+      src={
+        isChecked
+          ? '/assets/check_box_orange06.svg'
+          : isLogin
+            ? '/assets/check_box_gray08.svg'
+            : '/assets/check_box_gray06.svg'
+      }
+      alt={''}
+      width={width}
+      height={height}
+      onClick={handleCheck}
+      className="cursor-pointer"
+    />
+  );
+};
+
+export default CheckBox;
