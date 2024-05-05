b3c50c93417d2f48183c74f89f6ec486f12019fb
hyunn522 <tjgus52213@gmail.com>
2024-05-05 16:23:48 +0900

    🌈 Update(#24): add chip-cloud to fourth-step component

--- src/containers/onBoarding/FourthStep.tsx
+++ src/containers/onBoarding/FourthStep.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 
+import FourthChipCloud from '@/components/onBoarding/FourthChipCloud';
 import OnboardingFrame from '@/components/onBoarding/OnboardingFrame';
 
 import Image from 'next/image';
 
 const FourthStep = () => {
   return (
-    <OnboardingFrame
-      stepImg={'/assets/onboarding/fourth_step.png'}
-      title={'배움 나누기'}
-      subTitle={
-        '내가 배움 선배가 되어 가르치거나, 내가 배움 후배가 되어 배우거나!'
-      }
-      centerImg={
-        <Image
-          src="/assets/onboarding/fourth_center_img.png"
-          width={1076}
-          height={745}
-          className="mt-[124px]" // step에서부터 124px
-          alt={''}
-        />
-      }
-    />
+    <div className="flex flex-col justify-center items-center">
+      <OnboardingFrame
+        stepImg={'/assets/onboarding/fourth_step.png'}
+        title={'배움 나누기'}
+        subTitle={
+          '내가 배움 선배가 되어 가르치거나, 내가 배움 후배가 되어 배우거나!'
+        }
+        centerImg={<FourthChipCloud />}
+      />
+    </div>
   );
 };
 
