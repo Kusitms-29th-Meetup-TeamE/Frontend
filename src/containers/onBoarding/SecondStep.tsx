9d3871d0234720ff08e06d51e1cec2a85227adea
hyunn522 <tjgus52213@gmail.com>
2024-05-05 13:28:28 +0900

    📍 Feat(#24): add second-step component

--- src/containers/onBoarding/SecondStep.tsx
+++ src/containers/onBoarding/SecondStep.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
-import Button from '@/components/common-components/button/Button';
-
 import OnboardingFrame from '@/components/onBoarding/OnboardingFrame';
 
 import Image from 'next/image';
 
-const FirstStep = () => {
+const SecondStep = () => {
   return (
     <OnboardingFrame
       stepImg={
         <Image
-          src="/assets/onboarding/first_step.png"
+          src="/assets/onboarding/second_step.png"
           width={300}
           height={12}
           alt={''}
         />
       }
-      title={''}
-      subTitle={''}
+      title={'배움 나누기'}
+      subTitle={
+        '내가 배움 선배가 되어 가르치거나, 내가 배움 후배가 되어 배우거나!'
+      }
       centerImg={
         <Image
-          src="/assets/onboarding/first_center_img.png"
+          src="/assets/onboarding/second_center_img.png"
           width={1200}
           height={736}
           className="mt-[124px]" // step에서부터 124px
@@ -32,4 +32,4 @@ const FirstStep = () => {
   );
 };
 
-export default FirstStep;
+export default SecondStep;
