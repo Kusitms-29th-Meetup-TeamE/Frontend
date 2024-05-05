82ffbac7afcef922d3befdfb2fa1d45b3fdd201d
hyunn522 <tjgus52213@gmail.com>
2024-05-05 16:24:40 +0900

    ✅ Test(#24): test onboarding components

--- src/app/seohyun/page.tsx
+++ src/app/seohyun/page.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
+import Chip from '@/components/common-components/chip';
+
 import PointItem from '@/components/main/PointItem';
+import FourthChip from '@/components/onBoarding/FourthChip';
 
 import Notice from '@/containers/main/Notice';
 import Point from '@/containers/main/Point';
 import SubBannerSlider from '@/containers/main/SubBannerSlider';
+import FirstStep from '@/containers/onBoarding/FirstStep';
+import FourthStep from '@/containers/onBoarding/FourthStep';
+import SecondStep from '@/containers/onBoarding/SecondStep';
+import ThirdStep from '@/containers/onBoarding/ThirdStep';
 
 const TestPage = () => {
   return (
-    <div className="pr-[10px] pl-[10px]">
+    <div className="w-full pr-[10px] pl-[10px]">
       <div className="flex gap-2 p-2"></div>
+      {/* <FirstStep /> */}
+      {/* <SecondStep /> */}
+      {/* <ThirdStep /> */}
+      <FourthStep />
       <Notice />
       <div className="h-[30px]"></div>
       <SubBannerSlider
