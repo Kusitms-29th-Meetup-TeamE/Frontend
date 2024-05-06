7020e42c760a55bd6c70775d503806d86eaa0174
chamny20 <chamny20@naver.com>
2024-05-07 00:01:13 +0900

    ✅ Test(#26): third form test

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import FirstForm from '@/containers/signup/FirstForm';
 import SecondForm from '@/containers/signup/SecondForm';
+import ThirdForm from '@/containers/signup/ThirdForm';
 
 export default function SignUp() {
   const [step, setStep] = useState<number>(0);
@@ -11,6 +12,7 @@ export default function SignUp() {
     <main className="">
       <FirstForm />
       <SecondForm />
+      <ThirdForm />
       <div>
         <button>이전</button>
         <button>다음</button>
