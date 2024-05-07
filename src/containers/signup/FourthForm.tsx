3bfb11035a3534b3b7c55d07a5e5cd5ef723caa6
chamny20 <dge3179@ajou.ac.kr>
2024-05-07 16:45:28 +0900

    📍 Feat(#26): apply react-hook-form and add error-line

--- src/containers/signup/FourthForm.tsx
+++ src/containers/signup/FourthForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { BsPerson } from 'react-icons/bs';
 import { MdOutlineLock } from 'react-icons/md';
 import { TfiEmail } from 'react-icons/tfi';
@@ -7,11 +8,71 @@ import Input from '@/components/common-components/input';
 
 import SignUpTitle from '@/components/signUp/SignUpTitle';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import Image from 'next/image';
+import { z } from 'zod';
+
+const schema = z
+  .object({
+    name: z.string().min(2, { message: '이름은 2글자 이상이어야 합니다.' }),
+    password: z
+      .string()
+      .min(8, { message: '비밀번호는 8자 이상 20자 이하이어야 합니다.' })
+      .regex(/^(?=.*d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}$/, {
+        message: '비밀번호는 영문, 숫자를 포함해야 합니다.',
+      }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: '비밀번호는 8자 이상 20자 이하이어야 합니다.' })
+      .regex(/^(?=.*d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}$/, {
+        message: '비밀번호는 영문, 숫자를 포함해야 합니다.',
+      }),
+    // string(): 문자열 타입을 나타내며, 문자열에 대한 유효성 검사를 수행
+    // number(): 숫자 타입을 나타내며, 숫자에 대한 유효성 검사를 수행
+    // boolean(): 부울 타입을 나타내며, 참/거짓 값에 대한 유효성 검사를 수행
+    // array(): 배열 타입을 나타내며, 배열에 대한 유효성 검사를 수행
+    // object(): 중첩된 객체를 정의할 수 있으며, 객체에 대한 유효성 검사를 수행
+    // nullable(): 값이 null일 수 있는지 여부를 나타냄
+    // optional(): 값을 생략할 수 있는지 여부를 나타냄
+    // refine(): 사용자 정의 유효성 검사 함수를 정의
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: '비밀번호가 일치하지 않습니다.',
+  });
+type Schema = z.infer<typeof schema>;
+
+export type FourthFormProps = {
+  onSubmit: (data: any) => void;
+};
 
 export default function FourthForm() {
-  const [email, setEmail] = useState<string>('');
-  const [confirmNum, setConfirmNum] = useState<Number | null>(null);
+  // const { onSubmit } = props;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    setValue,
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmitForm = (data: Schema) => {
+    // onSubmit(data);
+    // TODO: 전역 상태에 데이터 저장 필요
+    console.log('name', getValues('name'));
+    console.log('pwd', getValues('password'));
+    console.log(getValues('confirmPassword'));
+  };
 
   return (
     <div className="m-auto border border-black w-full max-w-[800px] flex flex-col justify-center items-center">
@@ -25,36 +86,49 @@ export default function FourthForm() {
 
       <SignUpTitle title={'회원가입'} subTitle={'가입 정보를 입력해주세요.'} />
 
-      <div className="mt-[40px] flex flex-col justify-center gap-5 w-full max-w-[588px]">
-        <div className="">
+      <form
+        onSubmit={handleSubmit(onSubmitForm)}
+        className="mt-[40px] flex flex-col justify-center gap-5 w-full max-w-[588px]"
+      >
+        <div className="flex flex-col">
           <span className="pl-5 text-gray-11 text-h3">이름</span>
           <Input
             startIcon={<BsPerson />}
-            onChange={() => {
-              // console.log(emailInputRef.current?.value);
-            }}
             size="lg"
-            defaultValue={email}
+            // defaultValue={name}
             placeholder="이름을 입력해주세요."
             shape="square"
+            {...register('name')}
           />
+          <span className="pl-7 mt-[10px] mb-[14px] text-h5 h-[28px] flex">
+            {errors.name ? (
+              <span className="text-error-main">{errors.name.message}</span>
+            ) : (
+              <span className="text-gray-08" />
+            )}
+          </span>
         </div>
 
         <div className="flex flex-col">
           <span className="pl-5 text-gray-11 text-h3">비밀번호</span>
           <Input
             startIcon={<MdOutlineLock />}
-            onChange={() => {
-              // console.log(emailInputRef.current?.value);
-            }}
             size="lg"
-            defaultValue={confirmNum as number}
+            // defaultValue={pwd}
             placeholder="비밀번호를 입력해주세요."
             shape="square"
             type="password"
+            {...register('password')}
           />
-          <span className="pl-7 mt-[10px] mb-[14px] text-h5 text-gray-08">
-            8자 이상 32자 이하로 입력해주세요.
+
+          <span className="pl-7 mt-[10px] mb-[14px] text-h5 h-[28px] flex">
+            {errors.password ? (
+              <span className="text-error-main">{errors.password.message}</span>
+            ) : (
+              <span className="text-gray-08">
+                8자 이상 32자 이하로 입력해주세요.
+              </span>
+            )}
           </span>
         </div>
 
@@ -62,17 +136,24 @@ export default function FourthForm() {
           <span className="pl-5 text-gray-11 text-h3">비밀번호 확인</span>
           <Input
             startIcon={<MdOutlineLock />}
-            onChange={() => {
-              // console.log(emailInputRef.current?.value);
-            }}
             size="lg"
-            defaultValue={confirmNum as number}
+            // defaultValue={confirmPwd}
             placeholder="위에 기재한 비밀번화와 동일하게 입력해주세요."
             shape="square"
             type="password"
+            {...register('confirmPassword')}
           />
+          <span className="pl-7 mt-[10px] mb-[14px] text-h5 h-[28px] flex">
+            {errors.confirmPassword ? (
+              <span className="text-error-main">
+                {errors.confirmPassword.message}
+              </span>
+            ) : (
+              <span className="text-gray-08" />
+            )}
+          </span>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
