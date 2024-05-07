e3518b9b6dda85fa7e089dfe4195d5f905aff003
hyunn522 <tjgus52213@gmail.com>
2024-05-07 13:58:24 +0900

    📍 Feat(#22): add a page to redirect and send login request

---
+++ src/app/login/oauth2/callback/kakao/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import { kakaoLogin } from '@/api/login/kakaoLoginApi';
+import { useQuery } from '@tanstack/react-query';
+
+import { useRouter, usePathname } from 'next/navigation';
+
+const page = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [userInfo, setUserInfo] = useState<string>();
+
+  let kakaoCode: string = '';
+
+  if (typeof window !== 'undefined') {
+    kakaoCode = new URL(window.location.href).searchParams.get('code') || '';
+  }
+
+  if (kakaoCode) {
+    const { data } = useQuery({
+      queryKey: ['KAKAO_CODE', kakaoCode],
+      queryFn: () => kakaoLogin(kakaoCode),
+    });
+
+    useEffect(() => {
+      setUserInfo(data);
+    }, [data]);
+  } else {
+    router.replace('/login');
+    throw new Error('code is invalid');
+  }
+
+  useEffect(() => {
+    if (userInfo) {
+      // localStorage.setItem('userId', userInfo.userId?.toString());
+      // localStorage.setItem('nickname', userInfo.nickname);
+      console.log(userInfo);
+      router.replace('/');
+    }
+  }, [userInfo]);
+
+  return <div></div>;
+};
+
+export default page;
