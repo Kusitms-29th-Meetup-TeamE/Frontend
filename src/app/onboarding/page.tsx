88e085e2f6c0d22eeacc65ec7554a2b64c4d837b
hyunn522 <tjgus52213@gmail.com>
2024-05-08 09:27:23 +0900

    ✅ Test(#24): test onboarding components

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import FirstStep from '@/containers/onBoarding/FirstStep';
+import FourthStep from '@/containers/onBoarding/FourthStep';
+import SecondStep from '@/containers/onBoarding/SecondStep';
+import ThirdStep from '@/containers/onBoarding/ThirdStep';
+
+import Image from 'next/image';
+
+const page = () => {
+  return (
+    <div>
+      <Image
+        className="absolute top-10 left-[50px]"
+        src="/assets/ddoba_logo_text.svg"
+        alt=""
+        width={86}
+        height={30}
+      />
+      <FirstStep />
+      {/* <SecondStep /> */}
+      {/* <ThirdStep /> */}
+      {/* <FourthStep /> */}
+    </div>
+  );
+};
+
+export default page;
