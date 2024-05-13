722523f111d3b48004586ddf964fd07bc858bc31
chamny20 <dge3179@ajou.ac.kr>
2024-05-13 09:53:06 +0900

    🌈 Update(#43): modified import lines

--- src/containers/signup/FifthForm.tsx
+++ src/containers/signup/FifthForm.tsx
@@ -4,7 +4,7 @@ import Input from '@/components/common-components/input';
 import SelectBox from '@/components/common-components/select-box/SelectBox';
 
 import SearchAddressModal from '@/components/signUp/SearchAddressModal';
-import SignUpTitle from '@/components/signUp/SignUpTitle';
+import SignUpTitle from '@/components/signup/SignUpTitle';
 
 import { dayItems, monthItems, yearItems } from '@/constants/object';
 
