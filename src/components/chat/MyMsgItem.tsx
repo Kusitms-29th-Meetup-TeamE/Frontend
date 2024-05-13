50e228e12cd2428706a271607fccbc95a790620c
chamny20 <chamny20@naver.com>
2024-05-14 01:37:59 +0900

    🌈 Update(#44): add props

--- src/components/chat/MyMsgItem.tsx
+++ src/components/chat/MyMsgItem.tsx
@@ -1,10 +1,19 @@
-export const MyMsgItem = () => {
+export type MsgItemProps = {
+  message: string;
+  nickname?: string;
+  imageUrl?: string; // profile-image
+  time: string;
+};
+
+export const MyMsgItem = (data: MsgItemProps) => {
   return (
     <div className="inline-flex gap-3 items-end">
-      <span className="text-footer-regular text-gray-06">오후 12:14</span>
+      <span className="text-footer-regular text-gray-06">
+        {data.time ?? '오후 12:14'}
+      </span>
       <span className="max-w-[365px] bg-gray-03 px-[18px] py-2 rounded-[20px] rounded-tr-none text-gray-10 text-h5">
-        텍스트 한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이 텍스트
-        한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이
+        {data.message ??
+          '텍스트 한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이'}
       </span>
     </div>
   );
--- src/components/chat/OtherMsgItem.tsx
+++ src/components/chat/OtherMsgItem.tsx
@@ -1,25 +1,30 @@
+import { MsgItemProps } from './MyMsgItem';
+
 import Image from 'next/image';
 
-export const OtherMsgItem = () => {
+export const OtherMsgItem = (data: MsgItemProps) => {
   return (
     <div className="inline-flex flex-col gap-3">
       <div className="flex gap-3 items-center">
         <Image
-          src={'/assets/main/main_banner.png'}
+          src={data.imageUrl ?? '/assets/main/main_banner.png'}
           alt=""
           width={48}
           height={48}
           className="object-cover w-[48px] h-[48px] rounded-full"
         />
-        <span className="text-black text-body2">닉네임</span>
+        <span className="text-black text-body2">
+          {data.nickname ?? '채민이'}
+        </span>
       </div>
 
       <div className="flex gap-3 items-end">
         <span className="max-w-[365px] bg-primary-orange6 px-[18px] py-2 rounded-[20px] rounded-tl-none text-white text-h5">
-          텍스트 한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이
-          텍스트 한줄 서현이짱 최고 멋쟁이 텍스트 한줄 서현이짱 최고 멋쟁이
+          {data.message ?? '안녕하세요..'}
         </span>
-        <span className="text-footer-regular text-gray-06">오후 12:14</span>
+        <span className="text-footer-regular text-gray-06">
+          {data.time ?? '오후 12:14'}
+        </span>
       </div>
     </div>
   );
