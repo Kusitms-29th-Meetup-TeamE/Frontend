cdd328125b38ccbf227f2280f02bc0b11c512934
chamny20 <chamny20@naver.com>
2024-05-15 02:24:50 +0900

    📍 Feat(#51): connect chatrooms-api

--- src/app/chat/activity/page.tsx
+++ src/app/chat/activity/page.tsx
@@ -1,73 +1,80 @@
-import { RoomItem, RoomItemProps } from '@/components/chat/RoomItem';
+'use client';
+
+import { RoomItem } from '@/components/chat/RoomItem';
+
+import { useChatRoomsGroup } from '@/hooks/api/useChat';
 
 import { ChatRoom } from '@/containers/chat/ChatRoom';
 import { EmptyChat } from '@/containers/chat/EmptyChat';
 import { RoomList } from '@/containers/chat/RoomList';
 
-export const dummyData: RoomItemProps[] = [
-  {
-    myId: 0,
-    roomId: 2,
-    imageUrl: '/assets/main/main_banner.png',
-    title: '채민채민서현',
-    lastMeetingDate: 30,
-    appointmentDate: '4월 30일',
-    lastMessage: '서현이가 짱',
-    lastMessageTime: '오후 12:14',
-  },
-  {
-    myId: 0,
-    roomId: 2,
-    imageUrl: '/assets/main/main_banner.png',
-    title: '채민채민서현',
-    lastMeetingDate: 30,
-    appointmentDate: '4월 30일',
-    lastMessage: '서현이가 짱',
-    lastMessageTime: '오후 12:14',
-  },
-  {
-    myId: 0,
-    roomId: 2,
-    imageUrl: '/assets/main/main_banner.png',
-    title: '채민채민서현',
-    lastMeetingDate: 30,
-    appointmentDate: '4월 30일',
-    lastMessage: '서현이가 짱',
-    lastMessageTime: '오후 12:14',
-  },
-  {
-    myId: 0,
-    roomId: 2,
-    imageUrl: '/assets/main/main_banner.png',
-    title: '채민채민서현',
-    lastMeetingDate: 30,
-    appointmentDate: '4월 30일',
-    lastMessage: '서현이가 짱',
-    lastMessageTime: '오후 12:14',
-  },
-  {
-    myId: 0,
-    roomId: 2,
-    imageUrl: '/assets/main/main_banner.png',
-    title: '채민채민서현',
-    lastMeetingDate: 30,
-    appointmentDate: '4월 30일',
-    lastMessage: '서현이가 짱',
-    lastMessageTime: '오후 12:14',
-  },
-];
+// export const dummyData: RoomItemProps[] = [
+//   {
+//     myId: 0,
+//     roomId: 2,
+//     imageUrl: '/assets/main/main_banner.png',
+//     title: '채민채민서현',
+//     lastMeetingDate: 30,
+//     appointmentDate: '4월 30일',
+//     lastMessage: '서현이가 짱',
+//     lastMessageTime: '오후 12:14',
+//   },
+//   {
+//     myId: 0,
+//     roomId: 2,
+//     imageUrl: '/assets/main/main_banner.png',
+//     title: '채민채민서현',
+//     lastMeetingDate: 30,
+//     appointmentDate: '4월 30일',
+//     lastMessage: '서현이가 짱',
+//     lastMessageTime: '오후 12:14',
+//   },
+//   {
+//     myId: 0,
+//     roomId: 2,
+//     imageUrl: '/assets/main/main_banner.png',
+//     title: '채민채민서현',
+//     lastMeetingDate: 30,
+//     appointmentDate: '4월 30일',
+//     lastMessage: '서현이가 짱',
+//     lastMessageTime: '오후 12:14',
+//   },
+//   {
+//     myId: 0,
+//     roomId: 2,
+//     imageUrl: '/assets/main/main_banner.png',
+//     title: '채민채민서현',
+//     lastMeetingDate: 30,
+//     appointmentDate: '4월 30일',
+//     lastMessage: '서현이가 짱',
+//     lastMessageTime: '오후 12:14',
+//   },
+//   {
+//     myId: 0,
+//     roomId: 2,
+//     imageUrl: '/assets/main/main_banner.png',
+//     title: '채민채민서현',
+//     lastMeetingDate: 30,
+//     appointmentDate: '4월 30일',
+//     lastMessage: '서현이가 짱',
+//     lastMessageTime: '오후 12:14',
+//   },
+// ];
 
 export default function ChatActivity() {
+  const { data, isLoading } = useChatRoomsGroup();
+  // console.log('data:', data);
+
   return (
     <div className="w-full mx-auto pt-[40px] max-w-[1200px] flex">
       <RoomList
         title="활동 대화방"
         subTitle="활동 대화방에서 약속을 또 잡아보아요!"
       >
-        {dummyData.map((item, idx) => {
+        {data?.groupChatRoomResList.map((item, idx) => {
           return (
             <div key={idx} className="pr-[5px]">
-              <RoomItem data={item as RoomItemProps} />
+              <RoomItem data={item} />
             </div>
           );
         })}
--- src/app/chat/share/page.tsx
+++ src/app/chat/share/page.tsx
@@ -1,26 +1,21 @@
 import { OneRoomItem } from '@/components/chat/OneRoomItem';
 import { RoomItem } from '@/components/chat/RoomItem';
 
+import { useChatRoomsDirect } from '@/hooks/api/useChat';
+
 import { ChatRoom } from '@/containers/chat/ChatRoom';
 import { RoomList } from '@/containers/chat/RoomList';
 
-import { dummyData } from '../activity/page';
-
 export default function ChatShare() {
+  const { data, isLoading } = useChatRoomsDirect();
+  console.log('data:', data);
   return (
     <div className="w-full mx-auto pt-[40px] max-w-[1200px] border border-black flex">
       <RoomList
         title="배움 나누기 대화방"
         subTitle="1:1 대화를 통해 만남을 확정해보아요!"
       >
-        {/* <RoomItem />
-        <RoomItem />
-        <RoomItem />
-        <RoomItem />
-        <RoomItem />
-        <RoomItem />
-        <RoomItem /> */}
-        {dummyData.map((item, idx) => {
+        {data?.directChatRoomResList.map((item, idx) => {
           return (
             <div key={idx} className="pr-[5px]">
               <OneRoomItem data={item} />
