ec51104ff694f13a8eb3d90cebf2e2bd02bf7a6e
chamny20 <chamny20@naver.com>
2024-05-15 16:01:58 +0900

    🌈 Update(#51): apply store and update

--- src/app/chat/activity/page.tsx
+++ src/app/chat/activity/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { RoomItem } from '@/components/chat/RoomItem';
 
 import { useChatRoomsGroup } from '@/hooks/api/useChat';
@@ -7,6 +9,7 @@ import { useChatRoomsGroup } from '@/hooks/api/useChat';
 import { ChatRoom } from '@/containers/chat/ChatRoom';
 import { EmptyChat } from '@/containers/chat/EmptyChat';
 import { RoomList } from '@/containers/chat/RoomList';
+import { useChatStore } from '@/store/chatStore';
 
 // export const dummyData: RoomItemProps[] = [
 //   {
@@ -63,7 +66,28 @@ import { RoomList } from '@/containers/chat/RoomList';
 
 export default function ChatActivity() {
   const { data, isLoading } = useChatRoomsGroup();
-  // console.log('data:', data);
+  console.log('data:', data);
+
+  // const myId = data?.myId;
+
+  const { myId, setMyId } = useChatStore();
+
+  // useEffect(() => {
+  //   setMyId(data?.myId as number);
+  //   console.log('myId', myId);
+  // }, []);
+
+  const [groupRoomId, setGroupRoomId] = useState<number | null>(null);
+
+  const handleClick = (id: number) => {
+    // item.id가 들어옴
+    /*
+    1. roomId(id) 및 senderId(myId) 담기
+    2. 채팅방이 열려야 함 -> /topic/chatting/${chatroomId}을 위해 
+    3. roomId가 바뀔 때마다 ChatRoom이 바뀌어야 함
+
+    */
+  };
 
   return (
     <div className="w-full mx-auto pt-[40px] max-w-[1200px] flex">
@@ -72,8 +96,13 @@ export default function ChatActivity() {
         subTitle="활동 대화방에서 약속을 또 잡아보아요!"
       >
         {data?.groupChatRoomResList.map((item, idx) => {
+          // 여기서 item.id가 roomId
           return (
-            <div key={idx} className="pr-[5px]">
+            <div
+              key={idx}
+              className="mr-[5px]"
+              onClick={() => handleClick(item.id)}
+            >
               <RoomItem data={item} />
             </div>
           );
@@ -81,8 +110,7 @@ export default function ChatActivity() {
       </RoomList>
 
       <section className="flex-1">
-        {/* <EmptyChat /> */}
-        <ChatRoom />
+        {groupRoomId !== null ? <ChatRoom /> : <EmptyChat />}
       </section>
     </div>
   );
