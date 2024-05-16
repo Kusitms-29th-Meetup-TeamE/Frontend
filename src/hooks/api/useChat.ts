291d22a7361477c7fd251232cd35a9c6847a572f
chamny20 <dge3179@ajou.ac.kr>
2024-05-16 10:29:43 +0900

    🌈 Update(#51): add token querykey

--- src/hooks/api/useChat.ts
+++ src/hooks/api/useChat.ts
@@ -3,9 +3,9 @@ import { DirectChatRoomsResponse, GroupChatRoomsResponse } from '@/types/chat';
 import { getChatRoomsDirect, getChatRoomsGroup } from '@/api/chat';
 import { useQuery } from '@tanstack/react-query';
 
-export const useChatRoomsGroup = () => {
+export const useChatRoomsGroup = (accessToken?: string) => {
   const { data, isLoading, error } = useQuery<GroupChatRoomsResponse>({
-    queryKey: ['chatroomsGroup'],
+    queryKey: ['chatroomsGroup', accessToken],
     queryFn: () => getChatRoomsGroup(),
   });
   return { data, isLoading, error };
