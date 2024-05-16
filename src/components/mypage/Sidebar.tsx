aa9604ffa1aeaf97c7229cfc906f8dcd1cb7a8d2
chamny20 <dge3179@ajou.ac.kr>
2024-05-16 18:13:12 +0900

    ✅ Test(#72): component

---
+++ src/components/mypage/Sidebar.tsx
@@ -0,0 +1,14 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <div>프로필부분</div>
+
+      <div>
+        {/* 메뉴 부분 */}
+        <ul>나의 일정 확인하기</ul>
+        <ul>활동 참여 내역 보기</ul>
+        <ul>나의 일정 확인하기</ul>
+      </div>
+    </aside>
+  );
+}
