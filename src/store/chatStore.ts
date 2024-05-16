41ad962b9a4adc2cf6d2e92f1d5ce9ba572facdc
chamny20 <dge3179@ajou.ac.kr>
2024-05-16 10:29:58 +0900

    🌈 Update(#51): modified setter

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -6,6 +6,6 @@ interface ChatStoreState {
 }
 
 export const useChatStore = create<ChatStoreState>((set) => ({
-  myId: undefined,
-  setMyId: (id: number) => set({ myId: id }),
+  myId: -1,
+  setMyId: (id: number) => set((state) => ({ myId: id })),
 }));
