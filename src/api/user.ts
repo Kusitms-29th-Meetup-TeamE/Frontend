77979463d6267142c90d3142bfaac809ef435aeb
hyunn522 <tjgus52213@gmail.com>
2024-05-16 21:57:08 +0900

    📍 Feat(#53): add post-local-user fetching

--- src/api/user.ts
+++ src/api/user.ts
@@ -127,10 +127,57 @@ export const postEmailAuth = async (email: string) => {
     // 텍스트로 변환
     const text = new TextDecoder().decode(content?.value);
 
-    // console.log('응답 데이터:', text);
     return text;
   } catch (error) {
     console.error('오류 발생:', error);
+  }
+};
+
+// [login] 로컬 - 사용자 로그인 요청
+export const postLocalLogin = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    const res = await fetch(`${BASE_URL}/login/local`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error in Login! Status: ${res.status}`);
+    }
+
+    // 로그인
+    if (res.headers) {
+      let jwtToken = res.headers.get('Authorization');
+      jwtToken = jwtToken?.split(' ')[1] || ''; // Bearer 제거
+
+      if (jwtToken) {
+        sessionStorage.clear();
+        sessionStorage.setItem('accessToken', jwtToken);
+        return null;
+      }
+    }
+    const reader = res.body?.getReader();
+    const content = await reader?.read();
+
+    // 텍스트로 변환
+    const text = new TextDecoder().decode(content?.value);
+
+    return text;
+  } catch (error) {
+    console.error('Error fetching Local Login:', error);
+    throw error;
   }
 };
 
@@ -153,7 +200,6 @@ export const postOnboardingInfo = async (personalities: string[]) => {
     }
     return res;
   } catch (err) {
-    // console.error('Error fetching Onboarding Info:', err);
     throw err;
   }
 };
