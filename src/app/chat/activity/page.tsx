98519ceddef1d782be0ed2707a00981ebd7fc35f
chamny20 <dge3179@ajou.ac.kr>
2024-05-17 11:40:19 +0900

    📍 Feat(#66): routing chat-room-item

--- src/app/chat/activity/page.tsx
+++ src/app/chat/activity/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { RoomItem } from '@/components/chat/RoomItem';
 
@@ -11,82 +11,16 @@ import { EmptyChat } from '@/containers/chat/EmptyChat';
 import { RoomList } from '@/containers/chat/RoomList';
 import { useChatStore } from '@/store/chatStore';
 
-// export const dummyData: RoomItemProps[] = [
-//   {
-//     myId: 0,
-//     roomId: 2,
-//     imageUrl: '/assets/main/main_banner.png',
-//     title: '채민채민서현',
-//     lastMeetingDate: 30,
-//     appointmentDate: '4월 30일',
-//     lastMessage: '서현이가 짱',
-//     lastMessageTime: '오후 12:14',
-//   },
-//   {
-//     myId: 0,
-//     roomId: 2,
-//     imageUrl: '/assets/main/main_banner.png',
-//     title: '채민채민서현',
-//     lastMeetingDate: 30,
-//     appointmentDate: '4월 30일',
-//     lastMessage: '서현이가 짱',
-//     lastMessageTime: '오후 12:14',
-//   },
-//   {
-//     myId: 0,
-//     roomId: 2,
-//     imageUrl: '/assets/main/main_banner.png',
-//     title: '채민채민서현',
-//     lastMeetingDate: 30,
-//     appointmentDate: '4월 30일',
-//     lastMessage: '서현이가 짱',
-//     lastMessageTime: '오후 12:14',
-//   },
-//   {
-//     myId: 0,
-//     roomId: 2,
-//     imageUrl: '/assets/main/main_banner.png',
-//     title: '채민채민서현',
-//     lastMeetingDate: 30,
-//     appointmentDate: '4월 30일',
-//     lastMessage: '서현이가 짱',
-//     lastMessageTime: '오후 12:14',
-//   },
-//   {
-//     myId: 0,
-//     roomId: 2,
-//     imageUrl: '/assets/main/main_banner.png',
-//     title: '채민채민서현',
-//     lastMeetingDate: 30,
-//     appointmentDate: '4월 30일',
-//     lastMessage: '서현이가 짱',
-//     lastMessageTime: '오후 12:14',
-//   },
-// ];
-
 export default function ChatActivity() {
   const { data, isLoading } = useChatRoomsGroup();
-  console.log('data:', data);
 
-  // const myId = data?.myId;
-
-  const { myId, setMyId } = useChatStore();
-
-  // useEffect(() => {
-  //   setMyId(data?.myId as number);
-  //   console.log('myId', myId);
-  // }, []);
+  const { setMyId } = useChatStore();
 
   const [groupRoomId, setGroupRoomId] = useState<number | null>(null);
 
   const handleClick = (id: number) => {
-    // item.id가 들어옴
-    /*
-    1. roomId(id) 및 senderId(myId) 담기
-    2. 채팅방이 열려야 함 -> /topic/chatting/${chatroomId}을 위해 
-    3. roomId가 바뀔 때마다 ChatRoom이 바뀌어야 함
-
-    */
+    setMyId(data?.myId as number);
+    setGroupRoomId(id); // roomId 담아주기
   };
 
   return (
@@ -103,15 +37,18 @@ export default function ChatActivity() {
               className="mr-[5px]"
               onClick={() => handleClick(item.id)}
             >
-              <RoomItem data={item} />
+              <RoomItem data={item} isSelected={groupRoomId === item.id} />
             </div>
           );
         })}
       </RoomList>
 
       <section className="flex-1">
-        <ChatRoom />
-        {/* {groupRoomId !== null ? <ChatRoom /> : <EmptyChat />} */}
+        {groupRoomId !== null ? (
+          <ChatRoom roomId={groupRoomId} />
+        ) : (
+          <EmptyChat />
+        )}
       </section>
     </div>
   );
