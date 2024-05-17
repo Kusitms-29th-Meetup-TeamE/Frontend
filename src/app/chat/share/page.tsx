586122b4b2879dfab4bee0d16c13ae7dce1dd72f
chamny20 <dge3179@ajou.ac.kr>
2024-05-17 15:16:05 +0900

    📍 Feat(#66): complete chat-direct-room connecting

--- src/app/chat/share/page.tsx
+++ src/app/chat/share/page.tsx
@@ -1,15 +1,63 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
+import Skeleton from '@/components/common-components/skeleton';
+
 import { OneRoomItem } from '@/components/chat/OneRoomItem';
 
 import { useChatRoomsDirect } from '@/hooks/api/useChat';
 
+import { SOCKET_URL } from '@/api';
 import { ChatRoom } from '@/containers/chat/ChatRoom';
+import { EmptyChat } from '@/containers/chat/EmptyChat';
 import { RoomList } from '@/containers/chat/RoomList';
+import { useChatStore } from '@/store/chatStore';
+import { CompatClient, Stomp } from '@stomp/stompjs';
+
+import SockJS from 'sockjs-client';
 
 export default function ChatSharePage() {
   const { data, isLoading } = useChatRoomsDirect();
-  // console.log('data:', data);
+
+  const { setMyId } = useChatStore();
+
+  const [directRoomId, setDirectRoomId] = useState<number | null>(null);
+
+  const handleClick = (id: number) => {
+    setMyId(data?.myId as number);
+    setDirectRoomId(id); // roomId 담아주기
+  };
+
+  const [stompClient, setStompClient] = useState<CompatClient | null>(null);
+  const [isSocketLoading, setIsSocketLoading] = useState<boolean>(true); // 추가
+
+  const connectToWebSocket = () => {
+    const socket = new SockJS(`${SOCKET_URL}`);
+    const client = Stomp.over(socket);
+
+    client.connect(
+      {},
+      () => {
+        console.log('Connection success');
+        setIsSocketLoading(false);
+      },
+      () => {
+        console.log('Connection failed');
+        // setIsSocketLoading(false);
+      },
+    );
+
+    setStompClient(client);
+
+    return () => {
+      if (client) {
+        client.disconnect();
+      }
+    };
+  };
+
+  useEffect(connectToWebSocket, []);
 
   return (
     <div className="w-full mx-auto pt-[40px] max-w-[1200px] border border-black flex">
@@ -19,16 +67,33 @@ export default function ChatSharePage() {
       >
         {data?.directChatRoomResList.map((item, idx) => {
           return (
-            <div key={idx} className="mr-[5px]">
-              <OneRoomItem data={item} />
+            <div
+              key={idx}
+              className="mr-[10px]"
+              onClick={() => handleClick(item.id)}
+            >
+              {isLoading ? (
+                <Skeleton width={470} height={170} />
+              ) : (
+                <OneRoomItem
+                  data={item}
+                  isSelected={directRoomId === item.id}
+                />
+              )}
             </div>
           );
         })}
       </RoomList>
 
       <section className="flex-1">
-        {/* <EmptyChat /> */}
-        {/* <ChatRoom /> */}
+        {isSocketLoading ? (
+          // TODO: 로딩 컴포넌트 넣기
+          <div>로딩중입니다요</div>
+        ) : directRoomId !== null ? (
+          <ChatRoom roomId={directRoomId} stompClient={stompClient} />
+        ) : (
+          <EmptyChat />
+        )}
       </section>
     </div>
   );
--- src/components/chat/OneRoomItem.tsx
+++ src/components/chat/OneRoomItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { DirectChatRoom } from '@/types/chat';
 
 import clsx from 'clsx';
@@ -9,19 +11,29 @@ const bgStyle = {
   clicked: 'border-primary-orange6 bg-primary-orange1',
 };
 
-const appointmentStyle = {
-  gray: 'text-gray-06',
-  orange: 'text-primary-orange6',
-};
+export const OneRoomItem = (props: {
+  data: DirectChatRoom;
+  isSelected: boolean;
+}) => {
+  const { data, isSelected } = props;
 
-export const OneRoomItem = (props: { data: DirectChatRoom }) => {
-  const { data } = props;
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  const currentBgStyle = isSelected ? bgStyle.clicked : bgStyle.default;
 
   return (
-    <div className="border w-full max-w-[486px] rounded-[20px] flex flex-col">
+    <div
+      className={clsx(
+        'cursor-pointer border w-full max-w-[486px] rounded-[20px] flex flex-col',
+        currentBgStyle,
+      )}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="flex gap-5 p-5">
         <Image
-          src={data.imageUrl ?? '/assets/main/main_banner.png'}
+          src={'/assets/main/main_banner.png'}
+          // src={data.imageUrl ?? '/assets/main/main_banner.png'}
           width={76}
           height={76}
           alt=""
@@ -42,8 +54,6 @@ export const OneRoomItem = (props: { data: DirectChatRoom }) => {
           <div className="flex justify-between">
             <span className="max-w-[255px] text-h5 text-gray-08 overflow-hidden text-ellipsis whitespace-nowrap">
               {data.lastMessage}
-              오랜만에 보고 싶네요
-              하하ss하하ajslfiajelifjalsiejflaiejflaiefjiljalsdfjalefjaliejfellipsisaljsdlifjadijf
             </span>
             <span className="text-gray-06 text-h5">{data.lastChatTime}</span>
           </div>
