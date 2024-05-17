bf225074ab41a629e5bf1b4eb45cffc8aab127e9
hyunn522 <tjgus52213@gmail.com>
2024-05-18 00:18:47 +0900

    📍 Feat(#59): add learning-profile component

---
+++ src/components/share/LearningProfile.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import React from 'react';
+import { FaCirclePlus } from 'react-icons/fa6';
+
+import Chip from '../common-components/chip';
+
+import Image from 'next/image';
+
+const LearningProfile = () => {
+  const isMypage = false;
+  return (
+    <div className="relative max-w-[282px] w-full px-4 py-5 rounded-[20px] bg-gray-02">
+      {isMypage ? (
+        ''
+      ) : (
+        <Image
+          src={'/assets/onboarding/check.png'}
+          width={227}
+          height={227}
+          alt={''}
+          className="absolute top-[-13%] rounded-full"
+        />
+      )}
+      <div className="mt-[179px] mb-[18px] flex items-center justify-between">
+        <span className="text-black text-footer-bold">김복순</span>
+        <div className="w-fit h-fit rounded-[18.5px] flex gap-[10px] bg-white px-[14px] py-1 text-notification-chip-no text-gray-08">
+          <span>62세</span>
+          <span>|</span>
+          <span>남</span>
+          <span>|</span>
+          <span>불광동</span>
+        </div>
+      </div>
+      <ul className="flex flex-col gap-2">
+        <li className="flex items-center px-4 py-[13px] bg-white rounded-[20px]">
+          <Chip text="운동" className="mr-[10px]" />
+          <span className="text-body2 text-black">탁구</span>
+        </li>
+        <li className="flex items-center px-4 py-[13px] bg-white rounded-[20px]">
+          <Chip text="운동" className="mr-[10px]" />
+          <span className="text-body2 text-black">탁구</span>
+        </li>
+        {isMypage ? (
+          ''
+        ) : (
+          <li className="flex items-center px-4 py-[13px] bg-white rounded-[20px]">
+            <FaCirclePlus className="ml-[6px] mr-4 text-gray-06 w-5 h-5" />
+            <span className="text-notification-chip-yes text-gray-07">
+              경험 프로필 등록
+            </span>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default LearningProfile;
