786d897b31324f5307b04941bbe3a8c8bdf255c9
chamny20 <chamny20@naver.com>
2024-05-18 20:27:19 +0900

    🔨 Fix: hotfix test

--- src/api/user.ts
+++ src/api/user.ts
@@ -24,7 +24,7 @@ export const getKakaoToken = async (code: string) => {
   // 회원가입
   // console.log(res);
   // console.log(res.body);
-  console.log(res.json);
+  // console.log(res.json);
   // console.log(res.json());
   return res.json();
   //  catch (error) {
