2f0f3706b9597eabb25089778811d7d7db7c1ac2
chamny20 <chamny20@naver.com>
2024-05-19 16:45:57 +0900

    📍 Feat(#77): add mypage

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,19 @@
+import MyPageTitle from '@/components/mypage/MypageTItle';
+import Sidebar from '@/components/mypage/Sidebar';
+
+import LearnProfile from '@/containers/mypage/LearnProfile';
+
+export default function MyPage() {
+  return (
+    <div className="w-full m-auto max-w-[1200px] border border-black">
+      <Sidebar />
+      <div className="mt-[60px] inline-flex flex-col gap-[30px] ml-[282px] pl-6 border border-blue-500">
+        <MyPageTitle
+          title="배움 프로필 수정하기"
+          content="배움 나누기 활동을 통해 얻은 경험 내역을 확인해보세요"
+        />
+        <LearnProfile />
+      </div>
+    </div>
+  );
+}
