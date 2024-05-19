e51e373b240d8c96d9152e5901215f72b07880b8
hyunn522 <tjgus52213@gmail.com>
2024-05-20 02:16:57 +0900

    🌈 Update(#52): add hooks for get-all-activity

---
+++ src/hooks/api/useActivity.ts
@@ -0,0 +1,14 @@
+import { AllActivityProps, getAllActivity } from '@/api/join';
+import { useQuery } from '@tanstack/react-query';
+
+export const useAllActivity = ({
+  page,
+  agencyType,
+  personalities,
+}: AllActivityProps) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['ALL_ACTIVITY'],
+    queryFn: () => getAllActivity({ page, agencyType, personalities }),
+  });
+  return { data, isLoading, error };
+};
