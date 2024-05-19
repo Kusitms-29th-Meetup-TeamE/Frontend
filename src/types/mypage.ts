31b95f1d7071a78d65294ab100a92d303239b6f0
chamny20 <chamny20@naver.com>
2024-05-19 16:45:42 +0900

    📍 Feat(#77): add interface

---
+++ src/types/mypage.ts
@@ -0,0 +1,13 @@
+export interface MyPageInfoProps {
+  name: string;
+  email: string;
+  location: string;
+  imageUrl: string;
+}
+
+export interface LearnProfileProps {
+  title: string;
+  experienceType: string;
+  introduce?: string;
+  description: string;
+}
