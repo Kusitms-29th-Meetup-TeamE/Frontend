3066083f3821f3b60ec5178a6924ffafc031568a
chamny20 <chamny20@naver.com>
2024-05-19 19:24:48 +0900

    🌈 Update(#77): add routing about sidebar

--- src/components/mypage/Sidebar.tsx
+++ src/components/mypage/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { useGetMyPageInfo } from '@/hooks/api/useMyPage';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const sidebarStyle = {
   ul: 'text-body2 text-gray-09 w-full max-w-[282px] py-[18px] box-border text-center hover:bg-primary-orange1 hover:text-primary-orange6 hover:text-body4 cursor-pointer',
@@ -14,6 +15,8 @@ const sidebarStyle = {
 };
 
 export default function Sidebar() {
+  const router = useRouter();
+
   const [isHovered, setIsHovered] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
 
@@ -55,17 +58,28 @@ export default function Sidebar() {
       {/* 메뉴 부분 */}
       <div>
         <ul className={sidebarStyle.ul}>나의 일정 확인하기</ul>
+        <ul className={sidebarStyle.ul}>활동 참여 내역 보기</ul>
         <ul
           onClick={handleClick}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
-          <ul className={sidebarStyle.ul}>활동 참여 내역 보기</ul>
+          <ul
+            className={sidebarStyle.ul}
+            onClick={() => router.push('/mypage/learn')}
+          >
+            나의 배움 확인하기
+          </ul>
           {(isHovered || isClicked) && (
             <div className={sidebarStyle.liBox}>
               <li className={sidebarStyle.li}>배움 프로필 수정하기</li>
               <li className={sidebarStyle.li}>나의 후기 확인하기</li>
-              <li className={sidebarStyle.li}>배움 나눔 내역 보기</li>
+              <li
+                className={sidebarStyle.li}
+                onClick={() => router.push('/mypage/learn/reviews')}
+              >
+                배움 나눔 내역 보기
+              </li>
             </div>
           )}
         </ul>
