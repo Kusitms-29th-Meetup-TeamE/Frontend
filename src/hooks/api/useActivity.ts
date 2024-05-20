55c05ae8a50983335df30302cd4f35c9d0f4f9f9
hyunn522 <tjgus52213@gmail.com>
2024-05-20 20:59:23 +0900

    🌈 Update(#52): add get-activity-detail hook

--- src/hooks/api/useActivity.ts
+++ src/hooks/api/useActivity.ts
@@ -1,26 +1,39 @@
-import { AllActivityProps, getAllActivity, getLikedActivity } from '@/api/join';
+import {
+  AllActivityProps,
+  getActivityDetail,
+  getAllActivity,
+  getLikedActivity,
+} from '@/api/join';
 import { useQuery } from '@tanstack/react-query';
 
 export const useAllActivity = ({
   page,
-  agencyType,
+  agencyTypes,
   personalities,
 }: AllActivityProps) => {
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ['ALL_ACTIVITY', page],
-    queryFn: () => getAllActivity({ page, agencyType, personalities }),
+    queryFn: () => getAllActivity({ page, agencyTypes, personalities }),
   });
   return { data, isLoading, error, refetch };
 };
 
 export const useLikedActivity = ({
   page,
-  agencyType,
+  agencyTypes,
   personalities,
 }: AllActivityProps) => {
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ['LIKED_ACTIVITY', page],
-    queryFn: () => getLikedActivity({ page, agencyType, personalities }),
+    queryFn: () => getLikedActivity({ page, agencyTypes, personalities }),
   });
   return { data, isLoading, error, refetch };
+};
+
+export const useActivityDetail = (id: number) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['ACTIVITY_DETAIL', id],
+    queryFn: () => getActivityDetail(id),
+  });
+  return { data, isLoading, error };
 };
