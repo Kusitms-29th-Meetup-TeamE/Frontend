a13c04ecd77c5b5cbbd78a3523e79cb9c7b68856
hyunn522 <tjgus52213@gmail.com>
2024-05-20 20:18:15 +0900

    📍 Feat(#52): add hooks for get-liked-activity

--- src/hooks/api/useActivity.ts
+++ src/hooks/api/useActivity.ts
@@ -1,4 +1,4 @@
-import { AllActivityProps, getAllActivity } from '@/api/join';
+import { AllActivityProps, getAllActivity, getLikedActivity } from '@/api/join';
 import { useQuery } from '@tanstack/react-query';
 
 export const useAllActivity = ({
@@ -7,8 +7,20 @@ export const useAllActivity = ({
   personalities,
 }: AllActivityProps) => {
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['ALL_ACTIVITY'],
+    queryKey: ['ALL_ACTIVITY', page],
     queryFn: () => getAllActivity({ page, agencyType, personalities }),
+  });
+  return { data, isLoading, error, refetch };
+};
+
+export const useLikedActivity = ({
+  page,
+  agencyType,
+  personalities,
+}: AllActivityProps) => {
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ['LIKED_ACTIVITY', page],
+    queryFn: () => getLikedActivity({ page, agencyType, personalities }),
   });
   return { data, isLoading, error, refetch };
 };
