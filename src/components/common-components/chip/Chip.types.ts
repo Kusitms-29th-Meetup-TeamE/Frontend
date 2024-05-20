994a637373dca5feeb8096f3ba49e3c21f69cfa1
hyunn522 <tjgus52213@gmail.com>
2024-05-20 23:27:50 +0900

    🔨 Fix(#59): fix build error

--- src/components/common-components/chip/Chip.types.ts
+++ src/components/common-components/chip/Chip.types.ts
@@ -10,4 +10,6 @@ export type ChipProps = {
   handleSelect?: Dispatch<SetStateAction<string>>;
   isPersonality?: boolean;
   isActivity?: boolean;
+  isLearning?: boolean;
+  isSelected?: boolean;
 };
