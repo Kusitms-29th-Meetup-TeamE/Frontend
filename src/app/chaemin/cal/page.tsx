d27e56bfd20da0ce0133fd2ff1c0473a456ee3b4
chamny20 <chamny20@naver.com>
2024-05-21 02:35:47 +0900

    ✅ Test(#98): fetch tag-data (success)

--- src/app/chaemin/cal/page.tsx
+++ src/app/chaemin/cal/page.tsx
@@ -37,6 +37,11 @@ export default function MyCalendarTestPage() {
 
   console.log('toMeetList', toMeetList);
 
+  const dateList = useMemo(
+    () => toMeetList.map((appointment) => appointment.date),
+    [toMeetList],
+  );
+
   return (
     <div className="flex w-full border border-black">
       <Calendar
@@ -45,8 +50,30 @@ export default function MyCalendarTestPage() {
         onChange={() => handleDateChange}
         value={date}
         calendarType="gregory"
-        next2Label={null} // +1년 & +10년 이동 버튼 숨기기
-        prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
+        next2Label={null}
+        prev2Label={null}
+        tileContent={({ date, view }) => {
+          if (
+            view === 'month' &&
+            dateList.includes(moment(date).format('YYYY-MM-DD'))
+          ) {
+            const toMeetItems = toMeetList.filter(
+              (item) =>
+                moment(item.date).format('YYYY-MM-DD') ===
+                moment(date).format('YYYY-MM-DD'),
+            );
+            console.log('sisi', toMeetItems);
+            return (
+              <div className="flex justify-center items-center absoluteDiv">
+                {toMeetItems.map((item, index) => (
+                  <div key={index} className="appointment">
+                    {item.tag}
+                  </div>
+                ))}
+              </div>
+            );
+          }
+        }}
       />
     </div>
   );
