4e3d7805f56136e5a4a586713ea44ee4f072c220
chamny20 <dge3179@ajou.ac.kr>
2024-05-20 13:31:43 +0900

    🌈 Update(#89): add localstorage clear

--- src/components/common-components/common/Header.tsx
+++ src/components/common-components/common/Header.tsx
@@ -24,6 +24,7 @@ export default function Header({ isGuest }: { isGuest: boolean }) {
 
   const handleLogout = () => {
     sessionStorage.clear();
+    localStorage.clear();
     window.location.href = '/';
   };
 
