8312d0dbf4ea6ca971593c62b5a1418ae8159bc4
chamny20 <dge3179@ajou.ac.kr>
2024-05-21 17:13:05 +0900

    💄 Style(#96): add hover style

--- src/components/common-components/common/Header.tsx
+++ src/components/common-components/common/Header.tsx
@@ -12,10 +12,10 @@ import Link from 'next/link';
 
 const variants = {
   menubar: 'w-1/2 max-w-[760px] flex text-gray-11 justify-between',
-  centerMenu: 'text-h3 cursor-pointer',
+  centerMenu: 'text-h3 cursor-pointer hover:text-primary-orange6',
   rightBar: 'flex items-center gap-[40px] text-gray-07 text-body3',
   rightMenu:
-    'flex items-center text-gray-07 text-body3 hover:text-red-500 cursor-pointer',
+    'flex items-center text-gray-07 text-body3 hover:text-primary-orange6 cursor-pointer',
 };
 
 export default function Header({ isGuest }: { isGuest: boolean }) {
