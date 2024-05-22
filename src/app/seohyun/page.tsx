f99ae24137c248772f438af605553df6193fca79
hyunn522 <tjgus52213@gmail.com>
2024-05-22 18:53:07 +0900

    ✅ Test(#79): test spinner component

--- src/app/seohyun/page.tsx
+++ src/app/seohyun/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import Spinner from '@/components/common-components/spinner';
+
 import Notice from '@/containers/main/Notice';
 import Point from '@/containers/main/Point';
 import SubBannerSlider from '@/containers/main/SubBannerSlider';
@@ -8,22 +12,25 @@ import ThirdStep from '@/containers/onboarding/ThirdStep';
 
 const TestPage = () => {
   return (
-    <div className="w-full pr-[10px] pl-[10px]">
-      {/* <div className="flex gap-2 p-2"></div>
-   
-      <FourthStep />
-      <Notice />
-      <div className="h-[30px]"></div>
-      <SubBannerSlider
-        imgs={[
-          '/assets/main/subBanner1.svg',
-          '/assets/main/subBanner1.svg',
-          '/assets/main/subBanner1.svg',
-        ]}
-      />
-      <div className="h-[30px]"></div>
-      <Point /> */}
-    </div>
+    <>
+      <div className="w-full h-100% pr-[10px] pl-[10px]">
+        {/* <div className="flex gap-2 p-2"></div>
+ 
+    <FourthStep />
+    <Notice />
+    <div className="h-[30px]"></div>
+    <SubBannerSlider
+      imgs={[
+        '/assets/main/subBanner1.svg',
+        '/assets/main/subBanner1.svg',
+        '/assets/main/subBanner1.svg',
+      ]}
+    />
+    <div className="h-[30px]"></div>
+    <Point /> */}
+        <Spinner isLoading={true} />
+      </div>
+    </>
   );
 };
 
