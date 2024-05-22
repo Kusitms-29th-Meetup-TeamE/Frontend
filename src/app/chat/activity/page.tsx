7d0617286ec703549711d1a233c1ebba856995d2
chamny20 <chamny20@naver.com>
2024-05-22 20:43:42 +0900

    🌈 Update(#79): apply spinner

--- src/app/chat/activity/page.tsx
+++ src/app/chat/activity/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 
 import Skeleton from '@/components/common-components/skeleton';
+import Spinner from '@/components/common-components/spinner';
 
 import { RoomItem } from '@/components/chat/RoomItem';
 
@@ -90,8 +91,7 @@ export default function ChatActivity() {
 
       <section className="flex-1">
         {isSocketLoading ? (
-          // TODO: 로딩 컴포넌트 넣기
-          <div>로딩중입니다요</div>
+          <Spinner />
         ) : groupRoomId !== null ? (
           <ChatRoom
             roomInfo={roomInfo as GroupChatRoom}
