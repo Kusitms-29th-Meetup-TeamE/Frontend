f581ce7f5d1e8bfec5e45e83cfd33ebe4551e41d
hyunn522 <tjgus52213@gmail.com>
2024-05-22 18:51:57 +0900

    📍 Feat(#79): add spinner component

---
+++ src/components/common-components/spinner/Spinner.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SyncLoader } from 'react-spinners';
+
+import { clsx } from 'clsx';
+
+export type SpinnerProps = {
+  isLoading?: boolean;
+  className?: string;
+  color?: string;
+  size?: string;
+  message?: string;
+};
+
+const Spinner = ({
+  isLoading,
+  className,
+  color,
+  size,
+  message,
+}: SpinnerProps) => {
+  return (
+    <div
+      className={clsx(
+        'w-full h-full flex flex-col gap-8 justify-center items-center m-auto',
+        className,
+      )}
+    >
+      <SyncLoader
+        loading={isLoading && true}
+        color={color || '#FF7715'}
+        size={size || 20}
+        speedMultiplier={0.7}
+      />
+      <div className="text-body3">{message || '로딩 중입니다'}</div>
+    </div>
+  );
+};
+
+export default Spinner;
