0a24cb45ef3907773ae772e90b964a2b2fc8434b
chamny20 <dge3179@ajou.ac.kr>
2024-05-22 18:05:46 +0900

    🌈 Update(#92): apply metadata global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ import GlobalModal, {
 
 import Providers from './providers';
 
+export const metadata = {
+  // metadataBase: new URL(process.env.NEXT_PUBLIC_ORIGIN),
+  title: '또바',
+  description: '또바는 또바',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,6 @@ import SubBannerSlider from '@/containers/main/SubBannerSlider';
 
 import Image from 'next/image';
 
-export const metadata = {
-  // metadataBase: new URL(process.env.NEXT_PUBLIC_ORIGIN),
-  title: '또바',
-  description: '또바는 또바',
-};
-
 export default function Home() {
   return (
     <main className="flex flex-col gap-[160px]">
