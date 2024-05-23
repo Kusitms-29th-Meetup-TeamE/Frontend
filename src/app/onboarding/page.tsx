dce3a299887be5df5245bfb542cf59d38052b1eb
hyunn522 <tjgus52213@gmail.com>
2024-05-23 20:23:49 +0900

    🔨 Fix(#138): fix onboarding scroll issue

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
   const step = useStepStore((state) => state.currentStep);
 
   return (
-    <div>
+    <div className="absolute top-0 w-full h-screen">
       {step === 0 && <ZeroStep />}
       {step === 1 && <FirstStep />}
       {step === 2 && <SecondStep />}
