10d1fce2b1e7fa0f6905c4a45dfed7f6c8e6b944
chamny20 <dge3179@ajou.ac.kr>
2024-05-23 11:31:23 +0900

    ✅ Test: next.config.mjs test about cors

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,33 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: `www.gimhae.go.kr`,
+        hostname: 'www.gimhae.go.kr',
       },
       {
         protocol: 'https',
-        hostname: `ddoba.s3.ap-northeast-2.amazonaws.com`,
+        hostname: 'ddoba.s3.ap-northeast-2.amazonaws.com',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
 
