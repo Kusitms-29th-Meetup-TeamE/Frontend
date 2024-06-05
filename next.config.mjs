f351f25febdc106b9707a6f357d459ca88344488
chamny20 <chamny20@naver.com>
2024-06-06 00:34:03 +0900

    🌈 Update: add hostname

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: `k.kakaocdn.net`,
       },
+      {
+        protocol: 'http',
+        hostname: `t1.kakaocdn.net`,
+      },
     ],
   },
 };
