8b916be7b592e09c8181dc5ffcf18131ef7e7cd2
chamny20 <dge3179@ajou.ac.kr>
2024-06-07 10:48:40 +0900

    📍 Feat(#165): add strToDate function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,3 +16,22 @@ export const formatDate = (d: string | Date) => {
   if (!d) return '-';
   return moment(d).local().format('YYYY-MM-DD HH:mm:ss');
 };
+
+export const formatSimpleDate = (d: string) => {
+  if (!d) return '-';
+  let parts = d.split(' ');
+  return parts[0] + ' ' + parts[1] + ' ' + parts[2];
+};
+
+export const strToDate = (d: string) => {
+  if (!d) return null;
+
+  const dateRegex = /(\d{4})년 (\d{1,2})월 (\d{1,2})일/;
+  const match = d.match(dateRegex);
+
+  if (!match) return null;
+
+  const [, year, month, day] = match.map(Number);
+
+  return new Date(year, month - 1, day);
+};
